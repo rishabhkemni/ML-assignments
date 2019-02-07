031946917040fa0463377e2eeb101ff3fe404be2
rishabhkemni <32161396+rishabhkemni@users.noreply.github.com>
2019-02-07 18:48:58 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,16 @@
+#Question 1  Given list is
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+#Expected  output-:  [8,9,10,11,12]
+
+print(x[7:12])
+print('*'*70)
+print(x[7:-4])
+
+#Question 2
+print('*'*70)
+print(x[1::2])
+
+#Question 3
+print('*'*70)
+print(x[::4])
